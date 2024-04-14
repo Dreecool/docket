7987b8d9e6a6bf7b124245142a900b9577e9bd09
Dreecool <114856837+Dreecool@users.noreply.github.com>
2024-04-14 20:34:57 +0800

    Update login.js

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 
 export const setLog = createAsyncThunk("login", async (data) => {
     try {
-        const response = await Axios.post("http://localhost:3001/login", data);
+        const response = await Axios.post("https://docket-84ya.vercel.app/login", data);
         const token = response.data.tok
         const ID = response.data.ID
         Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
